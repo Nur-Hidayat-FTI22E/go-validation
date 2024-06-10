75ee60964ec60737afe27691aa721ce76f96ff35
Nur-Hidayat-FTI22E <105841115422@student.unismuh.ac.id>
2024-06-10 21:49:55 +0800

    Go Validation

--- validation_test.go
+++ validation_test.go
@@ -14,8 +14,10 @@ func TestValidation(t *testing.T) {
 	if validate == nil {
 		t.Error("Validasinya kosong")
 	}
-
 }
+
+//membuat variabel validate dengan pointer validator
+//memeriksa var validate apakah nilainya kosong atau tidak
 
 func TestValidationVariable(t *testing.T) {
 	validate := validator.New()
@@ -24,24 +26,34 @@ func TestValidationVariable(t *testing.T) {
 	if err := validate.Var(user, "required"); err != nil {
 		fmt.Println(err.Error())
 	}
-
 }
 
+//menambah variabel user bertipe data String
+//memeriksa kondisi dimana var user harus memiliki value, jika kosong maka error
+
 func TestValidationTwoVariables(t *testing.T) {
-	var (
-		Password        string
-		ConfirmPassword string
-	)
 	validate := validator.New()
 
-	fmt.Println("Create Your Password")
-	fmt.Scan(&Password)
-
-	fmt.Println("Confirm Your Password")
-	fmt.Scan(&ConfirmPassword)
+	Password := 0
+	ConfirmPassword := 0
 
 	if err := validate.VarWithValue(Password, ConfirmPassword, "eqfield"); err != nil {
 		fmt.Println(err.Error())
 	}
-
 }
+
+//menambah variabel password dan confirmpassword bertipe data int
+//memeriksa kondisi var Password dan ConfirmPassword dimana harus mempunyai value/nilai yang sama, jika tidak maka error
+
+func TestValidationMultipleTag(t *testing.T) {
+	validation := validator.New()
+
+	noHp := ""
+
+	if err := validation.Var(noHp, "required,numeric"); err != nil {
+		fmt.Println(err.Error())
+	}
+}
+
+//membuat variabel noHp bertipe data string
+//memeriksa var noHp yg dimana harus hanya memiliki value/nilai numeric walaupun di set dengan tipe data string, jika lain dari numeric atau valuenya kosong maka error
